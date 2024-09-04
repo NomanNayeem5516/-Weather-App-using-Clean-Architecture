94ab21b0b304bfca34ab9baa6d2638ee1da74ee5
NomanNayeem5516 <nomannayeem5516@gmail.com>
2024-09-04 16:55:17 +0600

    creating login page

---
+++ lib/screens/authScreens/login_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app_eith_clean_arc/helper/colors_helper.dart';
+import 'package:weather_app_eith_clean_arc/helper/dimensn_helper.dart';
+import 'package:weather_app_eith_clean_arc/helper/font_helper.dart';
+import 'package:weather_app_eith_clean_arc/helper/string_helper.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: ColorsHelper.primaryColor,
+        title: const Text(StringHelper.logIn),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+            top: DimensnHelper.dimensn_20,
+            left: DimensnHelper.dimensn_20,
+            right: DimensnHelper.dimensn_20),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: emailController,
+              decoration:
+                  const InputDecoration(hintText: StringHelper.enterEmail),
+            ),
+            const SizedBox(
+              height: DimensnHelper.dimensn_10,
+            ),
+            TextFormField(
+              controller: passwordController,
+              decoration:
+                  const InputDecoration(hintText: StringHelper.enterPassword),
+            ),
+            const SizedBox(
+              height: DimensnHelper.dimensn_20,
+            ),
+            SizedBox(
+              height: DimensnHelper.dimensn_50,
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: ColorsHelper.blueColor),
+                child: const Text(
+                  StringHelper.logIn,
+                  style: TextStyle(
+                      color: ColorsHelper.whiteColor,
+                      fontSize: FontHelper.dimensn_10),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/authScreens/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app_eith_clean_arc/helper/assets_helper.dart';
+import 'package:weather_app_eith_clean_arc/helper/dimensn_helper.dart';
+import 'package:weather_app_eith_clean_arc/screens/authScreens/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const LoginScreen()));
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: DimensnHelper.dimensn_200,
+          width: DimensnHelper.dimensn_200,
+          child: Image.asset(AssetsHelper.weatherIcon),
+        ),
+      ),
+    );
+  }
+}
